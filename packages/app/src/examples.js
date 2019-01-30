a3893654c8ee65d4092aa49372bb866050abd1be
Arthur Clemens <arthurclemens@gmail.com>
2019-01-30 18:30:05 +0100

    Add form examples

--- packages/app/src/examples.js
+++ packages/app/src/examples.js
@@ -10,6 +10,69 @@ const examples = [
 
 <div><p><span>text</span></p></div> <div><p><span>text</span></p></div>`,
 
+  // Form elements
+  `<!-- Form elements -->
+
+<form>
+  <p>
+    <input readonly class="textfield" type=text value="Company" />
+  </p>
+  <p>
+    <input required class="textfield" type=text name="name" placeholder="Your name" value="" />
+  </p>
+  <p>
+    <button type="submit" disabled>Send</button>
+  </p>
+</form>`,
+
+  // Tailwind Form
+  `<!-- Tailwind Form -->
+
+<form class="w-full" style="background:#fff; padding:20px;">
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Full Name
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-full-name" type="text" value="Jane Doe">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-username">
+        Password
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-username" type="password" placeholder="******************">
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3"></div>
+    <label class="md:w-2/3 block text-grey font-bold">
+      <input class="mr-2 leading-tight" type="checkbox">
+      <span class="text-sm">
+        Send me your newsletter!
+      </span>
+    </label>
+  </div>
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/3"></div>
+    <div class="md:w-2/3">
+      <button class="shadow bg-purple hover:bg-purple-light focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        Sign Up
+      </button>
+    </div>
+  </div>
+</form>
+
+<style>
+  @import 'https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css'
+</style>`,
+
+
   // Bootstrap button group
   `<!-- Bootstrap button group -->
 
