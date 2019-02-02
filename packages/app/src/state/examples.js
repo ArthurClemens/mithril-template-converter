cd7d517e1a2b557bb005c26013d3cd14d66e793a
Arthur Clemens <arthurclemens@gmail.com>
2019-02-02 23:57:28 +0100

    Minor

--- packages/app/src/state/examples.js
+++ packages/app/src/state/examples.js
@@ -226,13 +226,13 @@ const exampleData = [
  * Shuffles array in place.
  * @param {Array} a items An array containing the items.
  */
-function shuffle(a) {
+const shuffle = a => {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
-}
+};
 
 const data = shuffle(exampleData);
 
